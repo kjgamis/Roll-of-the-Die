dc651776da4f8f7689224a220bb184dbc734c53b
Karen <kjgamis@live.ca>
2018-03-22 22:43:55 -0400

    done

---
+++ permutation_totals.rb
@@ -0,0 +1,14 @@
+# roll_1 = (Random.rand(6) + 1)
+# puts roll_1
+
+# roll_2 = (Random.rand(6) + 1)
+# puts roll_2
+
+die_1 = [1, 2, 3, 4, 5, 6]
+die_2 = [1, 2, 3, 4, 5, 6]
+
+die_1.each do |roll_1|
+  die_2.each do |roll_2|
+    puts "Dice Roll: #{roll_1}, #{roll_2} Total: #{roll_1 + roll_2}"
+  end
+end
